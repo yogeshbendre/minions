472f6c56ee2b4b508d45753a27527c7c96b90ec0
Yogesh Bendre <ybendre@vmware.com>
2020-07-20 17:52:15 +0530

    add tools

--- vcst/rest/tkg/configure_wcp_tools.py
+++ vcst/rest/tkg/configure_wcp_tools.py
@@ -5,7 +5,7 @@
 import json
 import os
 import time
-from vcst.rest.tkg import WCPFetcher as wf
+from vcst.rest.tkg.WCPFetcher import WCPFetcher as wf
 
 #Params: vc, username, password, tmc_url, api_token, org_id, lcp_prefix, monitor_time_in_min, yaml_action, force_delete
 class Test:
